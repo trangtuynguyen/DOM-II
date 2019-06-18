400898f60ae50896a7529cf27100a493146a6b63
Trang Nguyen <ttn2127@columbia.edu>
2019-06-18 18:12:17 -0400

    second commit

--- js/index.js
+++ js/index.js
@@ -12,6 +12,13 @@ const pageName = document.querySelector('.intro h2');
 const topImg = document.querySelector('.intro img');
 console.log(navi);
 
+navi.forEach((nav)=>{
+    
+    nav.addEventListener('click', (event) =>{
+        event.preventDefault();
+    } )
+})
+
 navi[0].addEventListener('click', (event) =>{
    navi[0].style.color = 'red';
    pageName.textContent = 'Home';
@@ -35,4 +42,42 @@ navi[1].addEventListener('click', (event) =>{
     pageName.textContent = 'Contact';
     topImg.style.display = 'none';
  } )
+
+const p = document.querySelectorAll('p');
+
+p.forEach((paragraph)=>{
+    paragraph.addEventListener("copy", event => {
+        alert('your text has been copied');
+        paragraph.style.color = 'red';
+    })
+});
+
+
+const h2 = document.querySelectorAll('h2');
+
+h2.forEach((header)=>{
+    header.addEventListener("dblclick", event => {
+        header.style.color = 'green';
+    })
+});
+
+const mainNav = document.querySelector('.main-navigation');
+
+window.addEventListener('scroll',()=>{
+    mainNav.style.background = 'yellow';
+    
+});
+
+const destBtn = document.querySelector('.destination .btn button');
+const dest = document.querySelector('.destination p');;
+console.log(dest)
+
+dest.addEventListener('copy', event =>{
+    dest.style.color= 'blue';
+    dest.preventDefault();
+})
+
+
+
+
 
